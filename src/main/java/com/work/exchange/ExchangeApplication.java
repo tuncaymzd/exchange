2fc01cc4487c545aa33f920cabf94e3fc10524b4
tuncay <tuncay59600@gmail.com>
2020-07-23 11:42:45 +0200

    add init method for init database and basic test

--- src/main/java/com/work/exchange/ExchangeApplication.java
+++ src/main/java/com/work/exchange/ExchangeApplication.java
@@ -1,7 +1,11 @@
 package com.work.exchange;
 
+import com.work.exchange.models.StockExchange;
+import com.work.exchange.repository.StockExchangeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExchangeApplication {
@@ -10,4 +14,31 @@ public class ExchangeApplication {
 		SpringApplication.run(ExchangeApplication.class, args);
 	}
 
+	@Bean
+	/**
+	 * Init DATA
+	 *
+	 */
+	CommandLineRunner init(StockExchangeRepository stockExchangeRepository) {
+		return args -> {
+			// FIRST STOCK EXCHANGE
+			StockExchange stockExchange = new StockExchange();
+			stockExchange.setId(1);
+			stockExchange.setName("National Association of Securities Dealers Automated Quotations");
+			stockExchange.setShortName("NASDAQ");
+			stockExchange.setRegion("US");
+			stockExchange.setMarketPlace("NEW YORK");
+
+			// SECOND STOCK EXCHANGE
+			StockExchange stockExchange2 = new StockExchange();
+			stockExchange2.setId(2);
+			stockExchange2.setName("Cotation Assistée en Continu");
+			stockExchange2.setShortName("CAC 40");
+			stockExchange2.setRegion("FRANCE");
+			stockExchange2.setMarketPlace("PARIS");
+
+			stockExchangeRepository.save(stockExchange);
+			stockExchangeRepository.save(stockExchange2);
+		};
+	}
 }
--- src/test/java/com/work/exchange/ExchangeApplicationTests.java
+++ src/test/java/com/work/exchange/ExchangeApplicationTests.java
@@ -1,7 +1,15 @@
 package com.work.exchange;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ExchangeApplicationTests {
@@ -9,5 +17,34 @@ class ExchangeApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void givenStockExchangeDoesNotExists_whenStockExchangeInfoIsRetrieved_then404IsReceived() throws IOException {
+
+		//GIVEN
+		HttpUriRequest request = new HttpGet("http://localhost:8080/stockExchanges/100");
+
+		//WHEN
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		//THEN
+		assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_NOT_FOUND);
+
+	}
+
+	@Test
+	public void givenStockExchangeExists_whenStockExchangeInfoIsRetrieved_then200IsReceived() throws IOException {
+
+		//GIVEN
+		HttpUriRequest request = new HttpGet("http://localhost:8080/stockExchanges/1");
+
+		//WHEN
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		//THEN
+		assertEquals(response.getStatusLine().getStatusCode(), 200);
+	}
+
+
 
 }
